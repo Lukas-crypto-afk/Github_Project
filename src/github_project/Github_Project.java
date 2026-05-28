76db0352d548f5db0c3d5c2e14877bc4260b998b
14257069 <14257069@205-22.Fortis.local>
2026-05-28 09:10:27 +0200

    Isch lieben Phil

--- src/github_project/Github_Project.java
+++ src/github_project/Github_Project.java
@@ -14,6 +14,8 @@ public class Github_Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Leck aya");
         // TODO code application logic here
     }
     
